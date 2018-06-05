357a9f2ade1b095483ee7c627ec9197ba1be33dc
Chris Main <cmain@128technology.com>
2018-06-05 10:50:22 -0400

    Handle Prototype Visibility

--- src/model/LeafList.ts
+++ src/model/LeafList.ts
@@ -13,6 +13,7 @@ export default class LeafList implements ListLike, Statement, Searchable, Typed,
   public choiceCase: Case;
   public description: string;
   public identities: Identities;
+  public isPrototype: boolean;
   public isVisible: boolean;
   public keys: Set<string>;
   public maxElements: number;
